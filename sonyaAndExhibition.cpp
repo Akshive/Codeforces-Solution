6e13eb2c1d5164ac8c3f6241e245cbd8296e3f8b
Akshive Pandey <pandeyakshive97@gmail.com>
2019-03-23 17:59:02 +0530

    Create sonyaAndExhibition.cpp

---
+++ sonyaAndExhibition.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define _CRT_SECURE_NO_DEPRECATE
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+
+#define REP(i, a, b) \
+for (int i = int(a); i <= int(b); i++)
+#define TRvi(c, it) \
+for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) \
+for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) \
+for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 1000000000
+#define PB(a)\
+push_back(a)
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n, m;
+    cin>>n>>m;
+    int l, r;
+    REP(i,1,m)cin>>l>>r;
+    l = 0;
+    REP(i,1,n){
+        cout<<l;
+        l = 1-l;
+    }cout<<endl;
+    return 0;
+}
