553bcc573d91afc566ca23d510ab41ae08c573b6
Akshive Pandey <pandeyakshive97@gmail.com>
2019-03-24 19:56:49 +0530

    Create smallestWord.cpp

---
+++ smallestWord.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define _CRT_SECURE_NO_DEPRECATE
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+
+#define REP(i, a, b) \
+for (int i = int(a); i <= int(b); i++)
+#define TRvi(c, it) \
+for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) \
+for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) \
+for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 1000000000
+#define PB(a)\
+push_back(a)
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    string s;
+    cin>>s;
+    int size = s.size();
+    int a[size] = {0};
+    REP(i,1,size-1){
+        if(s[i] == 'a'){
+            a[i-1] += 1;
+            a[i] = 1;
+        }
+    }
+    REP(i,0,size-1)a[i] %= 2;
+    REP(i,0,size-1)cout<<a[i]<<" ";
+    return 0;
+}
